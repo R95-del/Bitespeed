4dc633506c0c2ed8355f87108039456d69ed1ebc
rishi <somani.rishi81@gmail.com>
2024-06-08 20:04:05 +0530

    change sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_06_06_194619) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
     t.string "phone_number"
     t.string "email"
