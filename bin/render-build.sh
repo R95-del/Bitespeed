0eeabaa2c06e4c17da6b5cd7eeaee45fa4e0017f
rishi <somani.rishi81@gmail.com>
2024-06-16 12:51:25 +0530

    Add custom build script and update render.yaml

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,3 +5,10 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+# Run database migrations
+bundle exec rails db:migrate
+
+# Start the server
+bundle exec rails server -b 0.0.0.0 -p $PORT
+
