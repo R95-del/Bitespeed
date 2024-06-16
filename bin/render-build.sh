bf8d7a54636c05b183d1ab645c3adea4fe57d0a1
rishi <somani.rishi81@gmail.com>
2024-06-16 13:01:16 +0530

    Update build script to include db setup and migration

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,9 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
+# Setup database
+bundle exec rails db:setup
+
 # Run database migrations
 bundle exec rails db:migrate
 
