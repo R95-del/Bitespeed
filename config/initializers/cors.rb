a7c19f15248c50e61ff084758db2dea1b8f95f94
rishi <somani.rishi81@gmail.com>
2024-06-16 11:43:44 +0530

    added cors gem

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,14 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://127.0.0.1:5500' # Replace with your front-end origin
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Access-Control-Allow-Origin']
+  end
+end
